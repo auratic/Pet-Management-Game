812e4ea98c59b7c184125e94f3a50d40b253b20a
unknown <mingwai148@gmail.com>
2024-01-08 07:35:23 +0800

    Improves Maze minigame GUI and responsiveness

--- public/Minigames/minigame4.js
+++ public/Minigames/minigame4.js
@@ -1,15 +1,74 @@
 
 var currentX = 0;
 var currentY = 0;
-var time = 10;
+var time = 30;
 
 var gameState = 0; // 0 = stop, 1 = start
 var gameMode = ''
 var coin = 0;
 
+// var coinSize = 5
+
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+function GUISetting () {
+  if(	window.screen.height >= 4096 || window.screen.width >= 4096 ) {
+      // (window.innerHeight > window.innerWidth) ? $("a").css({"font-size": "2em"}) : $("a").css({"font-size": "1em"});
+      $("h1").css({"font-size": "5em"});
+      $("h2").css({"font-size": "5em"});
+      $("h3").css({"font-size": "4em"});
+      $("h4").css({"font-size": "5em"});
+      $("label").css({"font-size": "5em"});
+      $("button").css({"font-size": "5em"});
+      $("table").css({"font-size": "4em"});
+  }else if(	window.screen.height >= 2560 || window.screen.width >= 2560 ) {
+      // (window.innerHeight > window.innerWidth) ? $("a").css({"font-size": "2em"}) : $("a").css({"font-size": "1em"});
+      $("h1").css({"font-size": "4em"});
+      $("h2").css({"font-size": "4em"});
+      $("h3").css({"font-size": "3em"});
+      $("h4").css({"font-size": "4em"});
+      $("label").css({"font-size": "4em"});
+      $("button").css({"font-size": "4em"});
+      $("table").css({"font-size": "4em"});
+  } else if(	window.screen.height >= 1920 || window.screen.width >= 1920 ) {
+      $("h1").css({"font-size": "3em"})
+      $("h3").css({"font-size": "2.5em"});
+      $("h4").css({"font-size": "3em"});
+      $("label").css({"font-size": "3em"});
+      $("button").css({"font-size": "3em"});
+  } else if(	window.screen.height >= 1280 || window.screen.width >= 1280 ) {
+      $("h1").css({"font-size": "2em"})
+      $("h2").css({"font-size": "2em"});
+      $("h3").css({"font-size": "1.5em"});
+      $("h4").css({"font-size": "2em"});
+      $("label").css({"font-size": "2em"});
+      $("button").css({"font-size": "2em"});
+      $("table").css({"font-size": "2em"});
+  } else if(	window.screen.height >= 1024 || window.screen.width >= 1024 ) {
+      $("h1").css({"font-size": "2em"})
+      $("h2").css({"font-size": "2em"});
+      $("h3").css({"font-size": "1.8em"});
+      $("h4").css({"font-size": "2em"});
+      $("label").css({"font-size": "2em"});
+      $("button").css({"font-size": "2em"});
+      $("table").css({"font-size": "2em"});
+  } else {
+      $("h1").css({"font-size": "2em"});
+      $("table").css({"font-size": "2em"});
+  }
+
+}
 
 $(document).ready(function(){
     
+  GUISetting();
+
   $('#retry').on('click', () => {
     window.location.reload(true);
   });
@@ -28,6 +87,8 @@ $(document).ready(function(){
 
     if(gameMode == "voice") {
       $('#voice-instruction').css({'display': 'flex'})
+      $("#result-display").css({'display': 'block'})
+      $("#tut-display").css({'display': 'none'})
       getMicAccess();
     } else {
       $('#keyboard-instruction').css({'display': 'flex'})
@@ -41,13 +102,12 @@ $(document).ready(function(){
         gameState = 1;
 
         let gameTime = setInterval(() => {
-          $('#point').html(coin);
-          if (time <= 0) {
+          if (time <= 0 || coin == 48) {
             gameOver();
             clearInterval(gameTime);
           } else {
-            $('#time').html(`${time} seconds`); 
             time--;
+            $('#time').html(`${formatTime(time)}`); 
           }
 
         }, 1000);
@@ -64,11 +124,11 @@ $(document).ready(function(){
 
     for (let i = 0; i < mazeSize ; i ++) {
       for (let j = 0; j < mazeSize ; j ++) {
-        if(i != 0 || j != 0) $(`#kruskal_y${i}x${j}`).append('<img src="coin-nobg.gif" style="height:100%; image-rendering: pixelated; padding-top:1px; object-fit:scale-down">')
+        if(i != 0 || j != 0) $(`#kruskal_y${i}x${j}`).append(`<img src="coin-nobg.gif" style="height:70%; image-rendering: pixelated; padding-top:1px;">`)
       }
     }
     let gridSize = (window.innerHeight > window.innerWidth) ? $('#maze-container').width() :  $('#maze-container').height();
-    $('#kruskal_y0x0').append('<img src="cat-idle-nobg.gif" style="height:100%; image-rendering: pixelated; padding-top:1px">')
+    $('#kruskal_y0x0').append(`<img src="cat-idle-nobg.gif" style="height:95%; image-rendering: pixelated; padding-top:1px">`)
     $('.small .grid .box > div').css({'width': `${gridSize / mazeSize - 20}px`, 'height': `${gridSize / mazeSize - 20}px`})
 
 
@@ -141,9 +201,11 @@ function updateMaze(direction) {
 }
 
 function gameOver () {
+  if(gameMode == 'voice') { coin = coin * 2; recognition.stop(); clearInterval(reqMic); }
+  $('#point').html(coin);
   $('#end-overlay').css({'display': 'flex'}); 
   gameState = 0; 
-  
+
   let getLeaderboard = new Promise ((res, rej) => {
     $.ajax({
       url: "/setLeaderboard",
@@ -269,13 +331,15 @@ navigator.webkitGetUserMedia ||
 navigator.mozGetUserMedia ||
 navigator.msGetUserMedia;
 
+var reqMic;
 function getMicAccess () {
-
+ 
   navigator.getUserMedia(
     { audio: true },
     (stream) => {
       micCheck = true;
-      setInterval(getMicAccess, 1000);
+      console.log("Mic granted")
+      reqMic = setInterval(getMicAccess, 5000);
     },
     (err) => {
       console.log(err);
@@ -288,7 +352,7 @@ function getMicAccess () {
   );
 
 }
-
+const commands = ["left", "right", "up", "down"]
 const mic = document.querySelector("#mic");
 
 recognition.onresult = (event) => {
@@ -304,10 +368,39 @@ recognition.onresult = (event) => {
   var confidence = event.results[0][0].confidence;
   var direction;
 
-  console.log(command);
-  $("#cmd-result").html(`${command}`);
+  // because a command can contain multiple words
+  // we need to split it.
+  let words = command.split(" ");
+  commandText = ""; // used for display to the users what speech is recognized
+  var commandDetected = "";
+  let isAdded = false; // to check if the word is to be highlighted. If true the highlighted word does not add multiple times
+  var index = 0; // In case there are multiple commands. We are to differentiate them with different id index
+  let textLength = 0;
+  var displayText = ""
 
-  switch (command) {
+  for (let i = 0; i < words.length; i++) {
+    textLength += words[i].length;
+    if (textLength >= 10) {
+      words.splice(i, words.length - i);
+      words.push("...");
+      return;
+    }
+  }
+
+  for (let i = 0 ; i < words.length; i++) {
+    if(commands.includes(words[i]) && !isAdded) {
+      isAdded = true;
+      displayText += `<span style="color:green">${words[i]}</span> `;
+      commandDetected = words[i];
+    } else {
+      displayText += `${words[i]} `
+    }
+  }
+
+  console.log(command);
+  $("#cmd-result").html(`${displayText}`);
+
+  switch (commandDetected) {
     case "left":
       direction = 'a';
       break;
@@ -323,10 +416,10 @@ recognition.onresult = (event) => {
   }
 
   if(
-    command == "left" || 
-    command == "right" || 
-    command == "up" || 
-    command == "down") 
+    commandDetected == "left" || 
+    commandDetected == "right" || 
+    commandDetected == "up" || 
+    commandDetected == "down") 
     { console.log(direction) ; updateMaze(direction); }
 
   // const color = event.results[0][0].transcript;
@@ -339,15 +432,34 @@ recognition.onspeechend = () => {
   console.log('speech end');
   recognition.stop();
 };
+// recognition.speechstart = () => {
+//   recognition.start();
+//   console.log("listening")
+// };
 
-mic.onmousedown = () => {
-    console.log('recognition starts')
-    $('#mic').css({"opacity": 0.5})
-    recognition.start();
-}
+recognition.start();
+// mic.onmousedown = () => {
+//     console.log('recognition starts')
+//     $('#mic').css({"opacity": 0.5})
+//     recognition.start();
+// }
 
-mic.onmouseup = () => {
-    console.log('recognition stops')
-    $('#mic').css({"opacity": 1})
-    recognition.stop();
+// mic.onmouseup = () => {
+//     console.log('recognition stops')
+//     $('#mic').css({"opacity": 1})
+//     recognition.stop();
+// }
+
+
+var playBGM = false;
+var BGM = new Audio('Assets/Audio/Fluffing-a-Duck.mp3');
+BGM.volume= 0.8;
+BGM.loop= true;
+
+window.onclick = () => {
+
+    if(!playBGM) {
+        BGM.play();
+        playBGM = true;
+    }
 }
