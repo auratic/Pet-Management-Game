7b90b23680e381ded5d90a88db8a6ff02b0b002e
unknown <mingwai148@gmail.com>
2024-01-08 03:59:32 +0800

    Remake Maze minigame

--- public/Minigames/minigame1.js
+++ public/Minigames/minigame1.js
@@ -1,206 +1,300 @@
-const video = document.getElementById('videoid');
 
-var vcanvas = document.querySelector("#vcanvas");
-vcanvas.setAttribute('width', video.clientWidth);
-vcanvas.setAttribute('height', video.clientHeight);
-var vctx = vcanvas.getContext("2d");
-
-var gameState = 0;
-var startCountdown;
-var predict;
-
-var finalHandSign;
-
-function checkCountdown () {
-
-   if(gameState == 1) {
-      console.log("gamestate = 1")
-      clearInterval(startCountdown);
-      
-      $("#counter").css({'display': 'block'});
-      let counter = 5
-      let countdown = setInterval(()=> {
-         console.log("enter interval" + counter)
-         counter--;
-         if(counter < 0) {
-            clearInterval(countdown);
-            gameState = 2;
-         } else {
-            $("#counter").html(counter);
-         }
-      },1000);
-   }
-}
-/*
-(function() {
-   console.log("load ht")
-   $.ajax({
-      type: 'post',
-      //url: 'db.js',
-      data: {
-          message: "helo",
-          action: "minigame",      
-          minigame: "1"
-      },
-      success: function (result) {
-          //alert(JSON.stringify(result));
-          handTrack = result;
-          setupHandtrack();
-          //console.log(result)
-          //setupHandtrack(result);
-          //return Promise.resolve("Success")
-      },
-      error: function(errMsg) {
-          alert(JSON.stringify(errMsg));
-      }
-  });
-})();
-*/
-
-function openCam(){
-    let All_mediaDevices=navigator.mediaDevices
-    if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
-       console.log("getUserMedia() not supported.");
-       return;
-    }
-    All_mediaDevices.getUserMedia({
-       audio: true,
-       video: true
-    })
-    .then(function(vidStream) {
-      
-       if ("srcObject" in video) {
-          video.srcObject = vidStream;
-       } else {
-          video.src = window.URL.createObjectURL(vidStream);
-       }
-       video.onloadedmetadata = function(e) {
-          video.play();
-       };
-    })
-    .catch(function(e) {
-       console.log(e.name + ": " + e.message);
-    });
- }
-
-function result(player1) {
-   gameState = 3;
-    let handsign = ["stone","paper","scissor"];
-    let player2 = handsign[(Math.floor(Math.random() * handsign.length))];
-    
-    if (player1 == "closed") {
-        if (player2 == "stone") {alert ("Draw !"); $("#handsign-img2").attr("src","/Minigames/Assets/stone.jpg");}
-        if (player2 == "paper") {alert ("Player 2 wins !"); $("#handsign-img2").attr("src","/Minigames/Assets/paper.jpg");}
-        if (player2 == "scissor") {alert ("Player 1 wins !"); $("#handsign-img2").attr("src","/Minigames/Assets/scissor.jpg");}
-
-        console.log(player1 +" and "+ player2)
-    } else if (player1 == "open") {
-        if (player2 == "stone"){ alert ("Player 1 wins  !"); $("#handsign-img2").attr("src","/Minigames/Assets/stone.jpg");}
-        if (player2 == "paper"){ alert ("Draw !"); $("#handsign-img2").attr("src","/Minigames/Assets/paper.jpg");}
-        if (player2 == "scissor") {alert ("Player 2 wins !"); $("#handsign-img2").attr("src","/Minigames/Assets/scissor.jpg");}
-        console.log(player1 +" and "+ player2)
-    } else if (player1 == "point") {
-        if (player2 == "stone") {alert ("Player 2 wins  !"); $("#handsign-img2").attr("src","/Minigames/Assets/stone.jpg");}
-        if (player2 == "paper") {alert ("Player 1 wins !"); $("#handsign-img2").attr("src","/Minigames/Assets/paper.jpg");}
-        if (player2 == "scissor") {alert ("Draw !"); $("#handsign-img2").attr("src","/Minigames/Assets/scissor.jpg");}
-        console.log(player1 +" and "+ player2)
-    }
+const blockTile = {
+   E: "E.png", EN: "EN.png", ES: "ES.png", ESN: "ESN.png", ESW: "ESW.png", ESWN: "ESWN.png", EW: "EW.png", EWN: "EWN.png", N: "N.png", S: "S.png", SN: "SN.png", SW:"SW.png", SWN: "SWN.png", W: "W.png", WN: "WN.png"
 }
 
+const mazeSize = 5;
 
-function drawGame(label) {
+const gameContainer = document.getElementById("game-container");
+const blockSize = $('#game-container').width() / mazeSize;
+const playerSize = blockSize / 2;
+const playerSpeed = 6;
+console.log(blockSize)
 
-   if(gameState == 2) {
-      $('#vcanvas').css({'display':'none'});
-      $('#videoid').css({'display':'block', 'height': '100%'});
-      clearInterval(predict);
-      result(finalHandSign);
-   } else {
-      if(label == "point") {
-         gameState = 1
-         $("#handsign-img").attr("src","/Minigames/Assets/scissor.jpg");
-         finalHandSign = "point";
+var gameMode = "keyboard";
+var gameState = 1
 
-      } else if(label == "closed") {
-         gameState = 1
-         $("#handsign-img").attr("src","/Minigames/Assets/stone.jpg")
-         finalHandSign = "closed";
+class Maze {
+   constructor (mazeSize, blockSize, blockTile, x, y, direction) {
+      console.log(mazeSize);
+      this.mazeSize = mazeSize
+      this.blockSize = blockSize
+      this.x = x
+      this.y = y
+      this.direction = direction
+      this.file = blockTile
 
-      } else if (label == "open") {
-         gameState = 1
-         $("#handsign-img").attr("src","/Minigames/Assets/paper.jpg")
-         finalHandSign = "open";
-
-      }
    }
 
+   generateBlock () {
+      return `
+         <div class="maze-block ${this.direction}" id="x${this.x}y${this.y}"
+                  style="
+                  width: ${this.blockSize};  
+                  height: ${this.blockSize};
+                  background: url('./Assets/Tile/${this.file}') no-repeat;
+                  background-size: contain;
+                  image-rendering: pixelated"
+         >
+         </div>`;
+   }
 
-    
+   collision () {
+      
+   }
 }
 
-/*
- *
- * Handtrackjs
- * 
- */
+class Player {
 
-//import * as handTrack from 'handtrackjs';
-//var handTrack = require('handtrackjs')
-//import * as handTrack from 'handtrackjs';
+   constructor () {
+      
+      this.x = 500,
+      this.y = 100,
+      this.width = 50,
+      this.height = 50
+      this.velocityX = 0;
+      this.velocityY = 0;
 
-const defaultParams = {
-   flipHorizontal: true,
-   outputStride: 16,
-   imageScaleFactor: 1,
-   maxNumBoxes: 1,
-   iouThreshold: 0.2,
-   scoreThreshold: 0.6,
-   modelType: "ssd320fpnlite",
-   modelSize: "large",
-   bboxLineWidth: "2",
-   fontSize: 17,
-};
+   }
 
-handTrack.load(defaultParams).then((lmodel) => {
-   model = lmodel;
-});
+   draw () {
+      this.x += this.velocityX;
+      this.y += this.velocityY;
+      return `<div style="
+                  position: absolute; 
+                  top: ${this.y}; left: ${this.x}; 
+                  width: ${this.width}; height: ${this.height};
+                  border: 5px solid black;
+                  z-index: 10">
+               </div>`;
+   }
+}
 
-//const model = await handTrack.load(defaultParams);
+const maze = [
+   new Maze(mazeSize, blockSize, blockTile.E, 0, 0, "E"),
+   new Maze(mazeSize, blockSize, blockTile.ESW, 0, 1, "E S W"),
+   new Maze(mazeSize, blockSize, blockTile.W, 0, 2, "W"),
+   new Maze(mazeSize, blockSize, blockTile.E, 0, 3, "E"),
+   new Maze(mazeSize, blockSize, blockTile.SW, 0, 4, "S W"),
+   new Maze(mazeSize, blockSize, blockTile.E, 1, 0, "E"),
+   new Maze(mazeSize, blockSize, blockTile.ESWN, 1, 1, "E S W N"),
+   new Maze(mazeSize, blockSize, blockTile.W, 1, 2, "W"),
+   new Maze(mazeSize, blockSize, blockTile.E, 1, 3, "E"),
+   new Maze(mazeSize, blockSize, blockTile.SWN, 1, 4, "S W N"),
+   new Maze(mazeSize, blockSize, blockTile.E, 2, 0, "E"),
+   new Maze(mazeSize, blockSize, blockTile.ESWN, 2, 1, "E S W N"),
+   new Maze(mazeSize, blockSize, blockTile.W, 2, 2, "W"),
+   new Maze(mazeSize, blockSize, blockTile.E, 2, 3, "E"),
+   new Maze(mazeSize, blockSize, blockTile.SWN, 2, 4, "S W N"),
+   new Maze(mazeSize, blockSize, blockTile.E, 3, 0, "E"),
+   new Maze(mazeSize, blockSize, blockTile.ESWN, 3, 1, "E S W N"),
+   new Maze(mazeSize, blockSize, blockTile.W, 3, 2, "W"),
+   new Maze(mazeSize, blockSize, blockTile.E, 3, 3, "E"),
+   new Maze(mazeSize, blockSize, blockTile.SWN, 3, 4, "S W N"),
+   new Maze(mazeSize, blockSize, blockTile.E, 4, 0, "E"),
+   new Maze(mazeSize, blockSize, blockTile.EWN, 4, 1, "E W N"),
+   new Maze(mazeSize, blockSize, blockTile.EW, 4, 2, "E W"),
+   new Maze(mazeSize, blockSize, blockTile.EW, 4, 3, "E W"),
+   new Maze(mazeSize, blockSize, blockTile.WN, 4, 4, "W N"),
+]
 
-//Run prediction and draws the video + predication box on a canvas
-function runDetection() {
-   
-   model.detect(video).then((predictions) => {
-      model.renderPredictions(predictions, vcanvas, vctx, video);
+const player = new Player();
 
-      if(predictions.length !== 0) {
-         //console.log(predictions)
-         //console.log("Class: " + predictions[0].class + " , Label: " + predictions[0].label)
+const keys = {
+   up: {
+		pressed: false
+   },
+   down: {
+		pressed: false
+   },
+	right: {
+		pressed: false
+	},
+	left: {
+		pressed: false
+	},
+}
 
-         drawGame(predictions[0].label);
+function animate() {
+   requestAnimationFrame(animate)
+   $("#game-container").empty();
+   let generateBlock = "";
+   let index = 0;
+
+   for (let i = 0 ; i < mazeSize ; i++) {
+      generateBlock += `<div class="row">`;
+      for (let j = 0 ; j < mazeSize ; j++) {
+         generateBlock += maze[index].generateBlock();
+         index++
       }
-   });
-}
+      generateBlock += `</div>`
+   }
 
+   $("#game-container").append(generateBlock);
+   $("#game-container").append(player.draw());
 
-document.addEventListener("DOMContentLoaded", async function() {
-//async function setupHandtrack () {
-   //console.log(handTrack)
+   // Movement with no collisions
+   // if(keys.up.pressed) { player.velocityY = -playerSpeed; }
+   // else if(keys.down.pressed) { player.velocityY = playerSpeed; }
+   // if(keys.left.pressed) { player.velocityX = -playerSpeed; }
+   // else if(keys.right.pressed) { player.velocityX = playerSpeed; }
+   // else { player.velocityX = 0; player.velocityY = 0; }
+
+   player.velocityX = 0; player.velocityY = 0; 
+   const square = document.querySelectorAll(".maze-block");
+   square.forEach((square1, index) => {
+
+      // Checking which maze block is user currently located
+      if(whereIsPlayer(square1)) {
+         console.log("player in " + square1.id)
+
+         if(keys.up.pressed) { 
+
+            if (square1.classList.contains("N")) {
+               player.velocityY = -playerSpeed; 
+            } else if (checkCollision(square1, "N")) {
+               player.velocityY = -playerSpeed; 
+            }
    
-   handTrack.startVideo(video).then(async (status) => {
-      if (status) {
-         console.log("Camera loaded");
-          
-         //const predictions =  await model.detect(video); 
+         } else if(keys.down.pressed) { 
+
+            if (square1.classList.contains("S")) {
+               player.velocityY = playerSpeed; 
+            } else if (checkCollision(square1, "S") ) {
+               player.velocityY = playerSpeed; 
+            }
+
+         } else if(keys.left.pressed) { 
+            
+            if (square1.classList.contains("W")) {
+               player.velocityX = -playerSpeed; 
+            } else if (checkCollision(square1, "W") ) {
+               player.velocityX = -playerSpeed; 
+            }
          
-         predict = setInterval(runDetection, 100);
-         startCountdown = setInterval(checkCountdown, 1000);
-      } else
-          console.log("HandtrackJs fail to start video")
-   });
-   
-   //handTrack.stopVideo(video); 
-   // stop camera input stream on the provided video tag.
+         } else if(keys.right.pressed) { 
+            
+            if (square1.classList.contains("E")) {
+               player.velocityX = playerSpeed; 
+            } else if (checkCollision(square1, "E") ) {
+               player.velocityX = playerSpeed; 
+            }
+         
+         } else { player.velocityX = 0; player.velocityY = 0; }
 
+      // Checking if user is located between two maze blocks
+      } else if (isInBetween(square1)) {
+         console.log("player in between " + square1.id)
+
+         if(keys.up.pressed) { 
+
+            if (checkCollision(square1, "N")) {
+               player.velocityY = -playerSpeed; 
+            }
+   
+         } else if(keys.down.pressed) { 
+
+            if (checkCollision(square1, "S") ) {
+               player.velocityY = playerSpeed; 
+            }
+
+         } else if(keys.left.pressed) { 
+            
+            if (checkCollision(square1, "W") ) {
+               player.velocityX = -playerSpeed; 
+            }
+         
+         } else if(keys.right.pressed) { 
+            
+            if (checkCollision(square1, "E") ) {
+               player.velocityX = playerSpeed; 
+            }
+         
+         }
+
+      }
+   })
+
+}
+
+animate(); 
+
+$(document).keydown(function(e) {
+	
+	if(gameMode == "keyboard" && gameState == 1) {
+		// Log the key code in the console
+		console.log("Key pressed: " + e.keyCode);
+		switch (e.keyCode) {
+			case 87:
+				keys.up.pressed = true;
+				// player.velocityY -= 10
+				break;
+			case 65:
+				keys.left.pressed = true;
+				// player.velocityX -= 10
+				break;
+			case 83:
+				keys.down.pressed = true;
+				break;
+			case 68:
+				keys.right.pressed = true;
+				break;
+		}
+	}
 });
 
+$(document).keyup(function(e) {
+	
+	switch (e.keyCode) {
+		case 87:
+			keys.up.pressed = false;
+			break;
+		case 65:
+			keys.left.pressed = false;
+			break;
+		case 83:
+			keys.down.pressed = false;
+			break;
+		case 68:
+			keys.right.pressed = false;
+			break;
+	}
+	
+});
+
+function whereIsPlayer(square) {
+   const squareRect = square.getBoundingClientRect();
+   return (
+      player.x >= squareRect.left &&
+      player.x + player.width <= squareRect.right &&
+      player.y >= squareRect.top &&
+      player.y + player.height <= squareRect.bottom
+    );
+}
+
+function isInBetween(square) {
+   const squareRect = square.getBoundingClientRect();
+   return ( 
+      (player.x <= squareRect.right && player.x + player.width >= squareRect.right && square.classList.contains("E")) ||
+      (player.x + player.width >= squareRect.left && player.x <= squareRect.left && square.classList.contains("W")) ||
+      (player.y <= squareRect.bottom && player.y + player.height >= squareRect.bottom && square.classList.contains("S")) ||
+      (player.y + player.height >= squareRect.top && player.y <= squareRect.top && square.classList.contains("N"))
+
+    );
+}
+
+function checkCollision (square, direction) {
+
+   const squareRect = square.getBoundingClientRect();
+   const gameRect = gameContainer.getBoundingClientRect();
+   if(direction == "N") {
+      return (player.y - playerSpeed > squareRect.top && player.y - playerSpeed > gameRect.top )
+   } else if(direction == "S") {
+      return (player.y + player.height + playerSpeed < squareRect.bottom && player.y + player.height + playerSpeed < gameRect.bottom)
+   } else if(direction == "W") {
+      return (player.x - playerSpeed > squareRect.left && player.x - playerSpeed > gameRect.left)
+   } else if(direction == "E") {
+      return (player.x + player.width + playerSpeed < squareRect.right && player.x + player.width + playerSpeed < gameRect.right)
+   } else return false
+
+}
